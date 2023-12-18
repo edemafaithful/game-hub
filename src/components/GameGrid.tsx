17ba5fa43dc807ea62716fe16ae0710bd4c01a2b
Edema Faithful <edemafaithful699@gmail.com>
2023-12-18 23:36:38 +0100

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,8 +14,8 @@ const GameGrid = () => {
 
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="20px"
-        spacing={10}
+        padding="18px"
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
