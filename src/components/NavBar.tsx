a865af8a616ac60ae0c1b55d890afcb826f3a6b2
Edema Faithful <edemafaithful699@gmail.com>
2023-12-20 21:56:17 +0100

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColourModeSwitch from "./ColourModeSwitch";
+import SearchIInput from "./SearchIInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
+    <HStack padding='10px'>
       <Image src={logo} boxSize="60px" />
+      <SearchIInput />
       <ColourModeSwitch />
     </HStack>
   );
